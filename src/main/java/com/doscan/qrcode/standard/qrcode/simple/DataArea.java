d4d7c65dba5ab34ecd4d45455dd6835af7dbc8f9
qingquan.du <qingquan.du@luckincoffee.com>
2019-01-13 08:46:00 +0800

    自序算法进行中

--- src/main/java/com/doscan/qrcode/standard/qrcode/simple/DataArea.java
+++ src/main/java/com/doscan/qrcode/standard/qrcode/simple/DataArea.java
@@ -28,20 +28,45 @@ public class DataArea {
             byte perByte = bits.get(i) ? (byte) 1 : 0;
             // 按照指定的排列规则，推演出下一个比特应该存在的位置
             int codewordIndex = 0;
+            int m,n;
+            int firstCol = m;
+            int secondCol = m - 1;
 
-            for(int m = sideNum - 1; m >= 0; ){
-                int firstCol = m;
-                int secondCol = m - 1;
+            int preM;
+            int preN;
+            int order = 1;
+            for(m = n = sideNum - 1; m >= 0; ){
+
+                // 迭代规则编写
+
+                if(dotData[m][n] == -1){
+                    //  -1 是未使用状态
+                        dotData[m][n] = perByte;
+                        i++;
+                        codewordIndex += 1;
+                        preM = m;
+                        preN = n;
+                }else{
+
+                }
+                if(order == 1){
+                    // up
+                    if(m == firstCol){
+                        m = secondCol;
+                    }else{
+
+                    }
+                }else{
+                    // down
+                }
+
                 // 需要先分块，按照每个模块的数值来计算  57页
 
 //                for(int n = sideNum - 1; n >= 0; ) {
 //
 //                    // 成功赋值一位
 //                    if (dotData[m][n] == -1) {
-//                        //  -1 是未使用状态
-//                        dotData[m][n] = perByte;
-//                        i++;
-//                        codewordIndex += 1;
+//
 //                    }
 //
 //                    // 以及存在其他区域的数据，寻找下一个的算法实现
