69ab16295a25fd2c22a39c2e1b79eadd67a92bef
qingquan.du <qingquan.du@luckincoffee.com>
2019-05-14 13:14:05 +0800

    修改密钥代码生成格式

--- src/main/java/com/duqingquan/encrypt/Demo.java
+++ src/main/java/com/duqingquan/encrypt/Demo.java
@@ -7,21 +7,19 @@ public class Demo {
 
     public static void main(String args[]) {
         Log.d("加解密演示 start...");
-        String sourceContent = "盼望着，盼望着，东风来了，春天的脚步近了。\n" +
-                "　　一切都像刚睡醒的样子，欣欣然张开了眼。山朗润起来了，水涨起来了，太阳的脸红起来了。\n" +
-                "　　小草偷偷地从土地里钻出来，嫩嫩的，绿绿的。园子里，田野里，瞧去，一大片一大片满是的。坐着，躺着，打两个滚，踢几脚球，赛几趟跑，捉几回迷藏。风轻悄悄的，草软绵绵的。";
-
+        String sourceContent = "dJLdCJiVnDvM9JUpsom9";
         int errNum = 0;
+
         for (int i = 0; i < 10; i++) {
 
             String encryptMessage = GateKeeper.getInstance()
                     .source(sourceContent)
-                    .key(336)
+                    .key(119)
                     .encryptInfo();
             Log.d("\"" + encryptMessage+"\",");
             String sourceInfo = GateKeeper.getInstance()
                     .messageInfo(encryptMessage)
-                    .key(336)
+                    .key(119)
                     .decryptInfo();
             if (!sourceInfo.equals(sourceContent)) {
                 errNum += 1;
